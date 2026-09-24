719d05e984d1e7b4791b90cebbf9caab4a4d7a7e
janhavi015 <janhaviss005gmail.com>
2026-09-24 13:59:19 +0530

    Add profile routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,40 @@
 import express from "express";
 
 import protectRoute from "../middleware/protectRoute.js";
+import upload from "../middleware/upload.middleware.js";
+
 import {
 	getUsersForSidebar,
 	searchUsers,
+	updateProfilePicture,
+	changePassword,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getUsersForSidebar);
+router.get(
+	"/",
+	protectRoute,
+	getUsersForSidebar
+);
 
-router.get("/search", protectRoute, searchUsers);
+router.get(
+	"/search",
+	protectRoute,
+	searchUsers
+);
+
+router.put(
+	"/profile-picture",
+	protectRoute,
+	upload.single("profilePic"),
+	updateProfilePicture
+);
+
+router.put(
+	"/change-password",
+	protectRoute,
+	changePassword
+);
 
 export default router;
